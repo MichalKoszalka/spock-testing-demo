55ddb6c64e710260ed6d9f553c46e26a45e3ed25
Michal Koszalka <michalko@practi.net>
2022-05-26 22:34:59 +0200

    initial commit

---
+++ src/main/java/com/example/spocktestingdemo/SpockTestingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spocktestingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockTestingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpockTestingDemoApplication.class, args);
+	}
+
+}
